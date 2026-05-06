8ce4bb78e36168af71fe5b3b2fb67795d552e87c
lowskydev <lowskydev@gmail.com>
2026-05-06 17:25:00 +0200

    add missing plugins to tracking

---
+++ .config/nvim/lua/plugins/terminal.lua
@@ -0,0 +1,28 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  config = function()
+    require("toggleterm").setup({
+      open_mapping = [[<c-\>]],
+      direction = "float",
+      float_opts = {
+        border = "rounded",
+      },
+      start_in_insert = true,
+      persist_mode = true,
+      autochdir = true,
+    })
+
+    vim.api.nvim_create_autocmd("TermOpen", {
+      pattern = "term://*",
+      callback = function()
+        local opts = { buffer = 0 }
+        vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], opts)
+        vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+        vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+        vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+        vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+      end,
+    })
+  end,
+}
